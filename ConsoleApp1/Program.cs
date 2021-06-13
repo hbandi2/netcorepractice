0cf899713466181628aca4b3c24e5897af9e7ac3
Harinatha Bandi <harinatha.bandi@gmail.com>
2021-06-13 13:57:38 -0500

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void HelloWorld(string name)
+        {
+            Console.WriteLine($"Hello {name} World!");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Name:");
+            string name = Console.ReadLine();
+            HelloWorld(name);
+        }
+    }
+}
